9673019c665cba276487266279b410e589b3c09a
demikhovr <demikhovr@gmail.com>
2019-06-26 00:03:34 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: false,
+    port: 1080,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  devtool: 'source-map',
+};
