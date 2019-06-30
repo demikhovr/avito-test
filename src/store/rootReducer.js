80617c384541456c06cd60bdada80a0098100b87
demikhovr <demikhovr@gmail.com>
2019-06-30 12:35:19 +0300

    update rootReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 import NameSpace from './NameSpace';
 import products from './products/Reducer/Reducer';
+import favorites from './favorites/Reducer/Reducer';
 import filters from './filters/Reducer/Reducer';
+import blinking from './blinking/Reducer/Reducer';
 
 export default combineReducers({
   [NameSpace.PRODUCTS]: products,
+  [NameSpace.FAVORITES]: favorites,
   [NameSpace.FILTERS]: filters,
+  [NameSpace.BLINKING]: blinking,
 });
