4295690a200f5ba75bf384d507e8890d6d408419
demikhovr <demikhovr@gmail.com>
2019-06-30 18:43:49 +0300

    update constants

--- src/constants.js
+++ src/constants.js
@@ -2,11 +2,23 @@ export const PRICE_LENGTH_TO_FORMAT = 5;
 export const PRICE_LOCALE = 'ru-RU';
 export const CHANGE_PIC_DELAY = 3000;
 export const INITIAL_PIC_INDEX = 0;
+export const PRICE_RANGE_RATE = 500;
 export const GEOCODE_URL = 'https://geocode-maps.yandex.ru/1.x/?format=json';
 export const GEOCODE_API_KEY = '6390cdfa-6556-43cb-a186-d1046ce806db';
+export const Filter = {
+  FAVORITES: 'is-favorite',
+  CATEGORY: 'category',
+  SORT: 'sort',
+  PRICE: 'price-change',
+};
 export const Category = {
   AUTO: 'auto',
   CAMERAS: 'cameras',
   IMMOVABLE: 'immovable',
   LAPTOPS: 'laptops',
+};
+export const SortingType = {
+  POPULAR: 'popular',
+  CHEAP_FIRST: 'cheap-first',
+  EXPENSIVE_FIRST: 'expensive-first',
 };
