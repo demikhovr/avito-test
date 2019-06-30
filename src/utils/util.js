490782c59351e56f73c40a7121cd0668a9cc975d
demikhovr <demikhovr@gmail.com>
2019-06-30 12:35:41 +0300

    add getRandomIndex()

--- src/utils/util.js
+++ src/utils/util.js
@@ -6,3 +6,17 @@ import {
 export const formatPrice = price => (`${price}`.length >= PRICE_LENGTH_TO_FORMAT
   ? price.toLocaleString(PRICE_LOCALE)
   : price);
+
+export const getRandomIndex = (arr, lastIndex) => {
+  if (arr.length === 1) {
+    return 0;
+  }
+
+  let randomIndex;
+
+  while (randomIndex === undefined || randomIndex === lastIndex) {
+    randomIndex = Math.floor(Math.random() * arr.length);
+  }
+
+  return randomIndex;
+}
