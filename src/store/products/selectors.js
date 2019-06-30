748407c0c9eb57c1e88cecee1056ca7ce3345593
demikhovr <demikhovr@gmail.com>
2019-06-30 19:14:28 +0300

    add getPriceRange selector

--- src/store/products/selectors.js
+++ src/store/products/selectors.js
@@ -2,3 +2,26 @@ import NameSpace from '../NameSpace';
 
 export const getLoadingState = state => state[NameSpace.PRODUCTS].isLoading;
 export const getProducts = state => state[NameSpace.PRODUCTS].products;
+export const getPriceRange = state => state[NameSpace.PRODUCTS].products
+  .reduce((obj, it) => {
+    const result = { ...obj };
+
+    if (it.price === undefined) {
+      return obj;
+    }
+
+    if (!obj.min && !obj.max) {
+      result.min = it.price;
+      result.max = it.price;
+    }
+
+    if (it.price > obj.max) {
+      result.max = it.price;
+    }
+
+    if (it.price < obj.min) {
+      result.min = it.price;
+    }
+
+    return result;
+  }, {});
